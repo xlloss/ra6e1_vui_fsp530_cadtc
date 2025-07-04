c7b96524ed5c7a5132489d1318734b30551d9afa
Slash.Huang <slash.linux.c@google.com>
2025-07-04 12:20:00 +0800

    [MODEL] rename demo model

--- src/Model/CybModel1.asm
+++ src/Model/CybModel1.asm
@@ -5,6 +5,6 @@
 .global uCYModel1End
 
 uCYModel1Begin:
-.incbin "../src/Model/Slash_Test_pack_WithTxtAndTri.bin"
+.incbin "../src/Model/Slash_Demo_Taiwan_Temp_Hum_pack_WithTxtAndTri.bin"
 uCYModel1End:
 
